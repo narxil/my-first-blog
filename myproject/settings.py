de03643b83022fdb34c4de0bf014ee84227acde2
narxil <32699519+narxil@users.noreply.github.com>
2017-10-11 10:25:46 +0200

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myprojectdb',
-        'USER': 'myprojectdbuser',
-        'PASSWORD': 'passworddb',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
